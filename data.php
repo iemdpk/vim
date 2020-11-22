0be64481bbafb2ddfbce6ae854c8c87f97f88ebe
Deepak Mishra <40403460+iemdpk@users.noreply.github.com>
2020-11-22 23:45:01 +0530

    Add files via upload

---
+++ data.php
@@ -0,0 +1,36 @@
+<?php
+function LoadJpeg($imgname)
+{
+    /* Attempt to open */
+    $im = @imagecreatefromjpeg($imgname);
+
+    /* See if it failed */
+    if(!$im)
+    {
+        /* Create a black image */
+        $im  = imagecreatetruecolor(150, 30);
+        $bgc = imagecolorallocate($im, 255, 255, 255);
+        $tc  = imagecolorallocate($im, 0, 0, 0);
+
+        imagefilledrectangle($im, 0, 0, 150, 30, $bgc);
+
+        /* Output an error message */
+        imagestring($im, 1, 5, 5, 'Error loading ' . $imgname, $tc);
+    }
+
+    return $im;
+}
+
+header('Content-Type: image/jpeg');
+
+$img = LoadJpeg('certifiate.jpeg');
+$font = realpath('arial.TTF');
+$color = imagecolorallocate($img, 51, 51, 102);
+imagefttext($img, 48,0,390,400,$color, $font, 'Deepak Mishra');
+
+
+
+
+imagejpeg($img);
+imagedestroy($img);
+?>
